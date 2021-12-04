a7d1584caa3b26ff0cc56dc70ac7bf8ecb68190c
Martin Terneborg <martinterneborg@protonmail.com>
2021-12-04 08:44:55 +0100

    Day 3 part 2 complete

--- day3/main.js
+++ day3/main.js
@@ -9,7 +9,6 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     .trim()
     .split("\n")
     .map((line) => line.split(""));
-  // console.log(bins);
 
   // part 1
   let [gamma, epsilon] = bins
@@ -43,12 +42,37 @@ fs.readFile("input.txt", "utf8", (err, data) => {
     )
     .map((rate) => rate.join(""))
     .map((rate) => parseInt(rate, 2));
-  console.log(gamma * epsilon);
+  console.log("part1: ", gamma * epsilon);
 
-  // // part 2
-  // let res2 = measurements.reduce(
-  //   (count, curr, i) => (measurements[i + 3] > curr ? count + 1 : count), // Skip the overlap in the sliding windows
-  //   0
-  // );
-  // console.log(res2);
+  // part 2
+
+  // separate array of binary nums into two arrays based on bit at position i.
+  let separate = (nums, i) =>
+    nums.reduce(
+      ([zeroes, ones], curr) =>
+        curr[i] == "0" ? [[...zeroes, curr], ones] : [zeroes, [...ones, curr]],
+      [[], []]
+    );
+
+  let filterOxygen = (nums, i = 0) => {
+    let [zeroes, ones] = separate(nums, i);
+    return nums.length == 1
+      ? nums[0].join("")
+      : zeroes.length > ones.length
+      ? filterOxygen(zeroes, i + 1)
+      : filterOxygen(ones, i + 1);
+  };
+  let filterCO2 = (nums, i = 0) => {
+    let [zeroes, ones] = separate(nums, i);
+    return nums.length == 1
+      ? nums[0].join("")
+      : zeroes.length > ones.length
+      ? filterCO2(ones, i + 1)
+      : filterCO2(zeroes, i + 1);
+  };
+
+  let oxygen = filterOxygen(bins);
+  let co2 = filterCO2(bins);
+
+  console.log("part2: ", parseInt(oxygen, 2) * parseInt(co2, 2));
 });
